5ef7c0373b2148ef9937ba679fe09d4d04a634ff
Ian Tenney <iftenney@gmail.com>
2017-01-11 00:06:24 +0000

    Update setup script

--- assignment/a0/cloud/setup.sh
+++ assignment/a0/cloud/setup.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+# Don't download stuff to the git repo, that's messy.
+pushd ${HOME}
+
 # Update packages
 sudo apt-get update
 sudo apt-get upgrade
@@ -16,4 +19,7 @@ TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.1
 ${HOME}/anaconda2/bin/pip install --upgrade pip
 ${HOME}/anaconda2/bin/pip install $TF_BINARY_URL
 ${HOME}/anaconda2/bin/jupyter notebook --generate-config
-cp support/jupyter_notebook_config.py ${HOME}/.jupyter/jupyter_notebook_config.py
+
+# Copy Jupyter config
+popd
+cp -v $(dirname $0)/support/jupyter_notebook_config.py ${HOME}/.jupyter/jupyter_notebook_config.py
